4b8501a30dfdd223112828bc9d5c1222f9f1846f
Sayaem Arafat <sayaemarafat360@gmail.com>
2026-04-29 21:45:52 +0600

    hfhfgd

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,20 +6,3 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
